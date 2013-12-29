19b559001718ca0fc47d05952182b7c2c2b3788b
Bradon Kanyid <rattboi@gmail.com>
2013-12-29 07:33:11 -0800

    Parsing pretty much the same stuff, but easier to work on now

--- backend/dirwalk.py
+++ backend/dirwalk.py
@@ -9,26 +9,43 @@ except:
     dirs = '.'
 
 re_year = re.compile("\((\d{4})\)")
+# TODO fix for ##.# issues
 re_issue = re.compile("#?[^\da-zA-Z](\d{1,3})[^\d]")
 re_tot_issues = re.compile("\([oO][fF][ _](\d{1,2})\)")
 re_vol = re.compile("v\d{1}")
 
-def print_reg(str, reg, file):
+parsers = {"Issue:": re_issue,
+           "Total:": re_tot_issues,
+           "Year :": re_year,
+           "Vol  :": re_vol}
+
+def get_reg(reg, file):
     r_all = reg.findall(file)
     try:
         r = r_all[0]
     except:
         r = None
-    if r is not None: 
-        print(str +  ": " + r)
+    return r
+
+def remove_str(orig, sub):
+    f = orig.find(sub)
+    if f == -1:
+        return orig
+    f_len = len(sub)
+    return orig[0:f] + orig[f+f_len:]
+
+def get_info(filename):
+    left_to_parse = filename
+    info = {}
+    for p in parsers.keys():
+        val = get_reg(parsers[p], left_to_parse)
+        if val is not None:
+            print(p + val)
+            left_to_parse = remove_str(left_to_parse,val)
+            print(left_to_parse)
 
 for root,subs,files in os.walk(dirs):
     for file in files:
         if file.lower().endswith("cbr") or file.lower().endswith("cbz"):
             print(root + "/" + file)
-            print_reg("Issue", re_issue, file)
-            print_reg("Total", re_tot_issues, file)
-            print_reg("Year ", re_year, file)
-            print_reg("Vol  ", re_vol, file)
-
-
+            get_info(file)
